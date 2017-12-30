7a46ef3325d87f5bfffd2166e1b086bdbd2aca11
vishalk17 <vishalkapadi95@gmail.com>
2017-12-30 17:14:46 +0000

    update paths from vendor to system/vendor
     we dont have separate vendor partition.In our case vendor is a part of system partition.

--- board/conn.mk
+++ board/conn.mk
@@ -1,16 +1,16 @@
 # GPS
 PRODUCT_COPY_FILES += \
-    $(VND_MTK_PATH)/configs/gps/slp_conf:vendor/etc/slp_conf \
-    $(VND_MTK_PATH)/configs/gps/gps.conf:vendor/etc/gps.conf \
-    $(VND_MTK_PATH)/configs/gps/agps_profiles_conf2.xml:vendor/etc/agps_profiles_conf2.xml
+    $(VND_MTK_PATH)/configs/gps/slp_conf:system/vendor/etc/slp_conf \
+    $(VND_MTK_PATH)/configs/gps/gps.conf:system/vendor/etc/gps.conf \
+    $(VND_MTK_PATH)/configs/gps/agps_profiles_conf2.xml:system/vendor/etc/agps_profiles_conf2.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
-    $(VND_MTK_PATH)/configs/bluetooth/auto_pair_devlist.conf:vendor/etc/bluetooth/auto_pair_devlist.conf \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.debug:vendor/etc/bluetooth/bt_stack.conf.debug \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_did.conf:vendor/etc/bluetooth/bt_did.conf \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf:vendor/etc/bluetooth/bt_stack.conf \
-    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.sqc:vendor/etc/bluetooth/bt_stack.conf.sqc
+    $(VND_MTK_PATH)/configs/bluetooth/auto_pair_devlist.conf:system/vendor/etc/bluetooth/auto_pair_devlist.conf \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.debug:system/vendor/etc/bluetooth/bt_stack.conf.debug \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_did.conf:system/vendor/etc/bluetooth/bt_did.conf \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf:system/vendor/etc/bluetooth/bt_stack.conf \
+    $(VND_MTK_PATH)/configs/bluetooth/bt_stack.conf.sqc:system/vendor/etc/bluetooth/bt_stack.conf.sqc
 
 # Telephony
 PRODUCT_COPY_FILES += \
@@ -19,7 +19,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(VND_MTK_PATH)/configs/wifi/p2p_supplicant_overlay.conf:vendor/etc/wifi/p2p_supplicant_overlay.conf \
-    $(VND_MTK_PATH)/configs/wifi/wpa_supplicant.conf:vendor/etc/wifi/wpa_supplicant.conf \
-    $(VND_MTK_PATH)/configs/wifi/wpa_supplicant_overlay.conf:vendor/etc/wifi/wpa_supplicant_overlay.conf
+    $(VND_MTK_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
+    $(VND_MTK_PATH)/configs/wifi/wpa_supplicant.conf:system/vendor/etc/wifi/wpa_supplicant.conf \
+    $(VND_MTK_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf
 
--- board/seccomp.mk
+++
@@ -1,4 +0,0 @@
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(VND_MTK_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(VND_MTK_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
